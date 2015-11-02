0e501b93d2807faa3fdc3669530e3fc33a354e5d
James <james@mercstudio.com>
2015-11-02 17:11:21 +0800

    renamed fix

--- fix-mysql.bat
+++ fix-mysql.bat
@@ -3,4 +3,3 @@ rem wget --no-check-certificate https://dev.mysql.com/get/Downloads/Connector-C/
 rem cd mysql
 rem ..\7zip\7z x mysql.zip
 gem install mysql2 -v '0.4.1' --platform=ruby -- '--with-mysql-dir="%CD%\mysql\mysql-connector-c-6.1.6-win32"'
-
